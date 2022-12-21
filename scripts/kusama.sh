3234b0fbb7e84dbd1942931569fe3baa4abbccac
Daniel Bigos <daniel.bigos@parity.io>
2022-12-21 16:46:48 +0100

    chore: use internal network config

--- scripts/kusama.sh
+++ scripts/kusama.sh
@@ -2,5 +2,5 @@
 POLKADOT_BINARY_PATH=~/local_bridge_testing/bin/polkadot \
 POLKADOT_PARACHAIN_BINARY_PATH=~/local_bridge_testing/bin/polkadot-parachain \
 POLKADOT_PARACHAIN_BINARY_PATH_FOR_ROCKMINE=~/local_bridge_testing/bin/polkadot-parachain \
-	~/local_bridge_testing/bin/zombienet --provider native spawn ./cumulus/zombienet/bridge-hubs/bridge_hub_rococo_local_network.toml
+	~/local_bridge_testing/bin/zombienet --provider native spawn ./network/bridge_hub_rococo_local_network.toml
 
